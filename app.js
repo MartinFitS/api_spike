88d3ce4826df13c9971d7cbab411a66c93b2a209
AmbrizAlberto <albertpoambez@gmail.com>
2024-09-02 08:16:42 -0600

    feat: api vrcl json added

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.post('/messages', async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
+
