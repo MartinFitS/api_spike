1e2fcaeedd56f0ca5267c6fb8ffb8abba11d6102
AmbrizAlberto <albertpoambez@gmail.com>
2024-10-01 13:13:09 -0600

    feat: users table created

---
+++ prisma/migrations/20241001190937_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'VETERINARY_OWNER', 'PET_OWNER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_firstName_lastName_idx" ON "users"("firstName", "lastName");
