83a7b6ed4d77e606aaa38ec809b41611e6f45a91
AmbrizAlberto <albertpoambez@gmail.com>
2024-10-11 12:20:27 -0600

    fix: vercel api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 require('dotenv').config();
 const app = require("./app");
 
+// Usar el puerto proporcionado por Vercel
 const port = process.env.PORT || 3000;
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
