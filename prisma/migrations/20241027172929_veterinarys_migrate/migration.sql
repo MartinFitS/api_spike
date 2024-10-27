2b2f5de3df36df9a1e2b3f8fc2a4bd802136a24c
MartinFitS <sernamartin46@gmail.com>
2024-10-27 12:20:46 -0600

    modify readme

---
+++ prisma/migrations/20241027172929_veterinarys_migrate/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "veterinarys" ADD COLUMN     "category" "Categories"[];
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -41,25 +41,31 @@ const createUser = async (req, res) => {
 
             const hashedPassword = await bcrypt.hash(password, 10);
 
+            // Subir la imagen a Cloudinary (si se envió una imagen)
             let imgUrl = null;
+            let imgPublicId = null;
             if (req.file) {
                 try {
                     const uploadResponse = await new Promise((resolve, reject) => {
                         cloudinary.uploader.upload_stream({ folder: 'usuarios' }, (error, result) => {
                             if (error) {
-                                console.error('Error al subir imagen a Cloudinary:', error); // Muestra el error completo
-                                reject(error); // Rechaza con el error completo
+                                console.error('Error al subir imagen a Cloudinary:', error);
+                                reject(error);
                             } else {
                                 resolve(result);
                             }
                         }).end(req.file.buffer);
                     });
+
                     imgUrl = uploadResponse.secure_url;
+                    imgPublicId = uploadResponse.public_id; // Asegúrate de que `public_id` se extrae correctamente
+                    console.log("Public ID:", imgPublicId); // Agregar log para verificar el valor
                 } catch (error) {
                     return res.status(500).json({ error: `Error al subir imagen a Cloudinary: ${error.message || error}` });
                 }
             }
 
+            // Crear el registro del usuario en la base de datos con `img` y `img_public_id`
             const newUser = await prisma.user.create({
                 data: {
                     firstName,
@@ -71,7 +77,8 @@ const createUser = async (req, res) => {
                     city,
                     number_int,
                     cp,
-                    img: imgUrl || 'default-image-url', // Usa una URL predeterminada si no hay imagen
+                    img: imgUrl || 'default-image-url',      // Usa una URL predeterminada si no hay imagen
+                    img_public_id: imgPublicId || null,       // Guarda el public_id de la imagen o null si no hay imagen
                 }
             });
 
@@ -84,15 +91,13 @@ const createUser = async (req, res) => {
     });
 };
 
-
 const createVeterinary = async (req, res) => {
     upload(req, res, async function (err) {
         if (err) {
             return res.status(500).json({ error: 'Error al cargar la imagen' });
         }
 
-        const { veterinarieName, street, email, phone, password, role, city, locality, cologne, number_int, cp, rfc } = req.body;
-
+        const { veterinarieName, street, email, phone, password, role, city, locality, cologne, number_int, cp, rfc, category } = req.body;
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[-_!¡?¿:@$!%*?&])[A-Za-z\d-_!¡?¿:@$!%*?&]{8,}$/;
         const rfcRegex = /^[A-Z]{3}\d{6}[A-Z0-9]{3}$/;
         const phoneRegex = /^\d{10}$/;
@@ -134,8 +139,19 @@ const createVeterinary = async (req, res) => {
 
             const hashedPassword = await bcrypt.hash(password, 10);
 
+            // Validar la categoría (debe ser un array y cada valor debe estar en el enum `Categories`)
+            const validCategories = ["NUTRITION", "RECREATION", "CARE"];
+            const categoryArray = Array.isArray(category) ? category : [category]; // Asegura que siempre sea un array
+
+            for (let cat of categoryArray) {
+                if (!validCategories.includes(cat)) {
+                    return res.status(400).json({ error: `Categoría inválida: ${cat}. Las categorías válidas son: ${validCategories.join(", ")}` });
+                }
+            }
+
             // Subir la imagen a Cloudinary (si se envió una imagen)
             let imgUrl = null;
+            let imgPublicId = null;
             if (req.file) {
                 try {
                     const uploadResponse = await new Promise((resolve, reject) => {
@@ -149,11 +165,13 @@ const createVeterinary = async (req, res) => {
                         }).end(req.file.buffer);
                     });
                     imgUrl = uploadResponse.secure_url;
+                    imgPublicId = uploadResponse.public_id; // Guardar el `public_id` de la imagen
                 } catch (error) {
                     return res.status(500).json({ error: `Error al subir imagen a Cloudinary: ${error.message || error}` });
                 }
             }
 
+            // Crear el registro de la veterinaria en la base de datos con `img`, `img_public_id`, y `category`
             const newVeterinarie = await prisma.veterinary.create({
                 data: {
                     veterinarieName,
@@ -169,6 +187,8 @@ const createVeterinary = async (req, res) => {
                     cp,
                     rfc,
                     img: imgUrl || 'default-image-url', // Usa una URL predeterminada si no se sube una imagen
+                    img_public_id: imgPublicId || null,   // Guarda el `public_id` de la imagen o null si no se sube
+                    category: categoryArray               // Guarda el array de categorías
                 }
             });
 
@@ -181,44 +201,109 @@ const createVeterinary = async (req, res) => {
     });
 };
 
+
 const updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { rfc, password,token, ...rest } = req.body; // Extraemos el password y el resto de los campos se agrupan en 'rest'
-        const updateData = { ...rest }; // Crea un objeto con los campos restantes
-        
-        // Si se incluye una contraseña, validar y encriptar
-        if (password) {
-            const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[-_!¡?¿:@$!%*?&])[A-Za-z\d-_!¡?¿:@$!%*?&]{8,}$/;
-            if (!passwordRegex.test(password)) {
-                return res.status(400).json({
-                    error: 'La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, un número y un símbolo especial'
+    upload(req, res, async function (err) {
+        if (err) {
+            return res.status(500).json({ error: 'Error al cargar la imagen' });
+        }
+
+        try {
+            const { id } = req.params;
+            const { rfc, password, token, newCategories, removeCategories, ...rest } = req.body;
+            const updateData = { ...rest };
+
+            // Validación de contraseña
+            if (password) {
+                const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[-_!¡?¿:@$!%*?&])[A-Za-z\d-_!¡?¿:@$!%*?&]{8,}$/;
+                if (!passwordRegex.test(password)) {
+                    return res.status(400).json({
+                        error: 'La contraseña debe tener al menos 8 caracteres, incluir una mayúscula, un número y un símbolo especial'
+                    });
+                }
+                const hashedPassword = await bcrypt.hash(password, 10);
+                updateData.password = hashedPassword;
+            }
+
+            // Manejo de imagen
+            if (req.file) {
+                const existingUser = await prisma.user.findUnique({
+                    where: { id: parseInt(id) },
+                    select: { img_public_id: true }
+                });
+
+                if (existingUser && existingUser.img_public_id) {
+                    await cloudinary.uploader.destroy(existingUser.img_public_id);
+                }
+
+                const uploadResponse = await new Promise((resolve, reject) => {
+                    cloudinary.uploader.upload_stream({ folder: 'usuarios' }, (error, result) => {
+                        if (error) {
+                            console.error('Error al subir imagen a Cloudinary:', error);
+                            reject(error);
+                        } else {
+                            resolve(result);
+                        }
+                    }).end(req.file.buffer);
+                });
+
+                updateData.img = uploadResponse.secure_url;
+                updateData.img_public_id = uploadResponse.public_id;
+            }
+
+            // Solo manejar `newCategories` y `removeCategories` si se proporciona `rfc` (es una veterinaria)
+            if (rfc !== undefined && rfc !== "") {
+                let categoriesToRemove = [];
+                let categoriesToAdd = [];
+                const validCategories = ["NUTRITION", "RECREATION", "CARE"];
+
+                if (removeCategories) {
+                    categoriesToRemove = typeof removeCategories === 'string' ? [removeCategories] : removeCategories;
+                    categoriesToRemove = categoriesToRemove.filter(cat => validCategories.includes(cat));
+                }
+
+                if (newCategories) {
+                    categoriesToAdd = typeof newCategories === 'string' ? [newCategories] : newCategories;
+                    categoriesToAdd = categoriesToAdd.filter(cat => validCategories.includes(cat));
+                }
+
+                // Obtener las categorías actuales de la veterinaria
+                const existingData = await prisma.veterinary.findUnique({
+                    where: { id: parseInt(id) },
+                    select: { category: true }
+                });
+
+                if (existingData) {
+                    // Crear la lista actualizada de categorías
+                    const updatedCategories = [
+                        ...existingData.category.filter(cat => !categoriesToRemove.includes(cat)),
+                        ...categoriesToAdd.filter(cat => !existingData.category.includes(cat))
+                    ];
+                    updateData.category = updatedCategories;
+                }
+            }
+
+            // Actualizar el usuario o veterinaria en la base de datos
+            if (rfc === undefined || rfc === "") {
+                // Actualiza el usuario si no se proporciona RFC
+                await prisma.user.update({
+                    where: { id: parseInt(id) },
+                    data: updateData
+                });
+            } else {
+                // Actualiza la veterinaria si se proporciona RFC
+                await prisma.veterinary.update({
+                    where: { id: parseInt(id) },
+                    data: updateData
                 });
             }
 
-            const hashedPassword = await bcrypt.hash(password, 10);
-            updateData.password = hashedPassword; // Solo agregamos la contraseña encriptada si se incluye
+            res.json("Usuario o veterinaria actualizado correctamente");
+        } catch (e) {
+            console.error(e);
+            res.status(500).json({ error: 'Error al actualizar el usuario o la veterinaria' });
         }
-
-        if (rfc === undefined || rfc === "") {
-            // Actualiza el usuario si no se proporciona RFC
-            await prisma.user.update({
-                where: { id: parseInt(id) },
-                data: updateData, // Usamos el objeto 'updateData' que tiene solo los campos enviados
-            });
-        } else {
-            // Actualiza la veterinaria si se proporciona RFC
-            await prisma.veterinary.update({
-                where: { id: parseInt(id) },
-                data: updateData, // Usamos el objeto 'updateData' que tiene solo los campos enviados
-            });
-        }
-
-        res.json("Usuario actualizado correctamente");
-    } catch (e) {
-        console.error(e);
-        res.status(500).json({ error: 'Error al actualizar el usuario' });
-    }
+    });
 };
 
 
