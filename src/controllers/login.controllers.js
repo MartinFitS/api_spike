fa726340fb7803e477712850d352ee7e9a4f511e
MartinFitS <sernamartin46@gmail.com>
2024-11-03 16:57:16 -0600

    login with verification mail

--- src/controllers/login.controllers.js
+++ src/controllers/login.controllers.js
@@ -9,6 +9,7 @@ const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
+        // Buscar en las tablas `user` y `veterinary`
         const userFromUsers = await prisma.user.findUnique({
             where: { email }
         });
@@ -19,16 +20,24 @@ const login = async (req, res) => {
 
         const user = userFromUsers || userFromVeterinary;
 
+        // Si el usuario no se encuentra
         if (!user) {
             return res.status(404).json({ message: 'Email no encontrado' });
         }
 
+        // Verificar si la cuenta está activa solo para usuarios en `User`
+        if (userFromUsers && userFromUsers.isActive === false) {
+            return res.status(403).json({ message: 'La cuenta no está verificada' });
+        }
+
+        // Verificar la contraseña
         const passwordMatch = await bcrypt.compare(password, user.password);
         
         if (!passwordMatch) {
             return res.status(401).json({ message: 'Contraseña incorrecta' });
         }
 
+        // Generar el token JWT
         const token = jwt.sign(
             { id: user.id, email: user.email }, 
             process.env.SECRET, 
@@ -42,5 +51,6 @@ const login = async (req, res) => {
         res.status(500).json({ message: 'Error en el servidor' });
     }
 };
+
 
 module.exports = {login}
